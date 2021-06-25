2a1a76735db32e570e37410ab7daf58f09b60fcf
Arthur Timoteo <arthur.timoteo@gmail.com>
2021-06-25 20:40:30 -0300

    Aula 04 - Landing

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2F%40anonymous%2Fgameplay-0c1f5213-f8b5-43f5-8592-8a4700ef1c2b';
+const SCOPE = 'identify%20email%20connections%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '857801900077613076';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+    REDIRECT_URI,
+    SCOPE,
+    RESPONSE_TYPE,
+    CLIENT_ID,
+    CDN_IMAGE   
+}
